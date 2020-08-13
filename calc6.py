e0d39ec0508da82c6bcf4082a906bf4b8514ecd6
Jonatan Missora <jonatanjmissora@gmail.com>
2020-08-13 17:17:12 -0300

    calc6

--- calc6.py
+++ calc6.py
@@ -21,6 +21,7 @@ from tkinter import *
 
 # estetica GUI
 # poner separacione de miles y millones
+# poner el simbolo de la operacion en curso
 # estetica de codigo
 
 #*************** CONSTANTES y VARIABLES *******************
@@ -28,15 +29,10 @@ operacion=""
 primer_numero="0"
 
 fnt = ("Arial", 15)
-
+"""
 #*************** FUNC FLOAT_CHECK *************************
 def check_imput(chr):
-	
 	pass
-
-	
-
-
 
 #*************** FUNC FLOAT_CHECK *************************
 def float_check(num):
@@ -44,7 +40,7 @@ def float_check(num):
 		return num
 	else:
 		return int(num)
-
+"""
 #*************** FUNC SHOW ********************************
 def show(chr):
 	global operacion
@@ -69,49 +65,17 @@ def borrar():
 	pantalla.set("0")
 	primer_numero = "0"
 
-#*************** FUNC SUMA ********************************	
-def suma():
-	global primer_numero
-	primer_numero = pantalla.get() + "+"
-	pantalla.set("0")
-"""
-#*************** FUNC RESTA *******************************
-def resta():
-	global numero_almacenado
-	numero_almacenado = pantalla.get() + "-"
-	pantalla.set("0")
+#*************** OPERACIONES ******************************
+def operacion(simbol):
+	global primer_numero	
+	if simbol == "+":
+		primer_numero = pantalla.get()
+		pantalla.set("0")
+	if simbol == "=":
+		segundo_numero = pantalla.get()
+		pantalla.set(int(segundo_numero) + int(primer_numero))
 
-#*************** FUNC MULTI ******************************* 	
-def multi():
-	global numero_almacenado
-	numero_almacenado = pantalla.get() + "*"
-	pantalla.set("0")
 
-#*************** FUNC DIV ********************************* 	
-def divi():
-	global numero_almacenado
-	numero_almacenado = pantalla.get() + "/"
-	pantalla.set("0")
-"""
-#*************** FUNC EL_RESULTADO ************************
-def resultado():
-	global primer_numero_
-	operacion = primer_numero[-1]
-	print(primer_numero)
-	
-	if operacion == "+":
-		resultado = int(pantalla.get()) + int(primer_numero[:-1])
-
-	"""elif operacion == "-":
-		resultado = int(primer_numero[:-1]) - int(pantalla.get())
-	elif operacion == "*":
-		resultado = int(primer_numero[:-1]) * int(pantalla.get())
-	elif operacion == "/":
-		resultado = int(primer_numero[:-1]) / int(pantalla.get())
-	"""
-
-	pantalla.set(float_check(resultado))
-	
 root=Tk()
 root.title("CALCULADORA")
 root.resizable(False, False)
@@ -129,25 +93,25 @@ visor.config(bg="black", fg="#03f943", justify="right")
 Button(win, text="7", font=fnt, width=5, height=2, command=lambda:show("7")) .grid(row=1, column=0, sticky=W+E)
 Button(win, text="8", font=fnt, width=5, height=2, command=lambda:show("8")) .grid(row=1, column=1, sticky=W+E)
 Button(win, text="9", font=fnt, width=5, height=2, command=lambda:show("9")) .grid(row=1, column=2, sticky=W+E)
-Button(win, text="/", font=fnt, width=5, height=2, command=lambda:divi())	  .grid(row=1, column=3, sticky=W+E)
+Button(win, text="/", font=fnt, width=5, height=2, command=lambda:operacion("/"))	  .grid(row=1, column=3, sticky=W+E)
 Button(win, text="CE", font=fnt, width=5, height=5, command=borrar)   		  .grid(row=1, column=4, rowspan=2, sticky=W+E)
 
 #*************** FILA 2 ***********************************
 Button(win, text="4", font=fnt, width=5, height=2, command=lambda:show("4")).grid(row=2, column=0, sticky=W+E)
 Button(win, text="5", font=fnt, width=5, height=2, command=lambda:show("5")).grid(row=2, column=1, sticky=W+E)
 Button(win, text="6", font=fnt, width=5, height=2, command=lambda:show("6")).grid(row=2, column=2, sticky=W+E)
-Button(win, text="*", font=fnt, width=5, height=2, command=lambda:multi())	 .grid(row=2, column=3, sticky=W+E)
+Button(win, text="*", font=fnt, width=5, height=2, command=lambda:operacion("*"))	 .grid(row=2, column=3, sticky=W+E)
 
 #*************** FILA 3 ***********************
 Button(win, text="1", font=fnt, width=5, height=2, command=lambda:show("1"))  .grid(row=3, column=0, sticky=W+E)
 Button(win, text="2", font=fnt, width=5, height=2, command=lambda:show("2"))  .grid(row=3, column=1, sticky=W+E)
 Button(win, text="3", font=fnt, width=5, height=2, command=lambda:show("3"))  .grid(row=3, column=2, sticky=W+E)
-Button(win, text="-", font=fnt, width=5, height=2, command=lambda:resta())	   .grid(row=3, column=3, sticky=W+E)
-Button(win, text="=", font=fnt, width=5, height=5, command=lambda:resultado()).grid(row=3, column=4, rowspan=2, sticky=W+E)
+Button(win, text="-", font=fnt, width=5, height=2, command=lambda:operacion("-"))	   .grid(row=3, column=3, sticky=W+E)
+Button(win, text="=", font=fnt, width=5, height=5, command=lambda:operacion("=")).grid(row=3, column=4, rowspan=2, sticky=W+E)
 
 #*************** FILA 4 ***********************
 Button(win, text="0", font=fnt, width=8, height=2, command=lambda:show("0")).grid(row=4, column=0, columnspan= 2, sticky=W+E)
 Button(win, text=",", font=fnt, width=5, height=2, command=lambda:show(".")).grid(row=4, column=2, sticky=W+E)
-Button(win, text="+", font=fnt, width=5, height=2, command=lambda:suma())	 .grid(row=4, column=3, sticky=W+E)
+Button(win, text="+", font=fnt, width=5, height=2, command=lambda:operacion("+"))	 .grid(row=4, column=3, sticky=W+E)
 
 root.mainloop()
